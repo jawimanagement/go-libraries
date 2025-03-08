1459c663dc55d7055d8902b2f0255e3d0c274643
mustofaonly <khoirulmustofa.nu@gmail.com>
2025-03-08 19:35:57 +0700

    empty passworrd

--- models/UserModel.go
+++ models/UserModel.go
@@ -11,7 +11,7 @@ type UserModel struct {
 	Name             NullString `gorm:"column:name" json:"name"`
 	Email            NullString `gorm:"column:email" json:"email"`
 	UserName         NullString `gorm:"column:username" json:"username"`
-	Password         NullString `gorm:"column:password" json:"password"`
+	Password         NullString `gorm:"column:password,omitempty" json:"password"`
 	RoleID           int        `gorm:"column:role_id" json:"role_id"`
 	PhoneCountryCode NullString `gorm:"column:phone_country_code" json:"phone_country_code"`
 	Phone            NullString `gorm:"column:phone" json:"phone"`
