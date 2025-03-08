9f4de7a204855c2bd58959a41df72c1a4d0768e1
mustofaonly <khoirulmustofa.nu@gmail.com>
2025-03-08 21:08:36 +0700

    city model fix

--- models/CityModel.go
+++ models/CityModel.go
@@ -3,13 +3,13 @@ package models
 import "gorm.io/gorm"
 
 type CityModel struct {
-	CityID        	NullString `gorm:"column:city_id;primary_key" json:"city_id"`
-	CityName      	NullString `gorm:"column:city_name" json:"city_name"`
-	ProvID	  		NullString `gorm:"column:prov_id" json:"prov_id"`
+	CityID   NullString `gorm:"column:city_id;primary_key" json:"city_id"`
+	CityName NullString `gorm:"column:city_name" json:"city_name"`
+	ProvID   NullString `gorm:"column:prov_id" json:"prov_id"`
 }
 
 func (p *CityModel) TableName() string {
-	return "city"
+	return "cities"
 }
 
 func (p *CityModel) BeforeCreate(tx *gorm.DB) (err error) {
